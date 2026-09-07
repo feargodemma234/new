9154002357a844458c1d727ba29e7a190ffcc009
feargodemma234 <feargodemma56@gmail.com>
2026-09-07 20:50:47 +0100

    Delete index.py

--- api/index.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from streamlit.web import cli as stcli
-import sys
-import os
-
-def handler(request):
-    sys.argv = ["streamlit", "run", "app.py", "--server.port", os.environ.get("PORT", "8501"), "--server.address", "0.0.0.0", "--server.headless", "true"]
-    stcli.main()
